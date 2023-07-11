046ede07b12e04c50524c48fac50447c3701c73a
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2023-07-11 11:05:19 +0200

    directive for

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
   // import PostContainer from './components/PostContainer.vue';
   // import CounterOption from './components/CounterOption.vue';
   // import CounterComposition from './components/CounterComposition.vue';
-  import ExampleSelective from './components/ExampleSelective.vue';
+  // import ExampleSelective from './components/ExampleSelective.vue';
+  import ExampleFor from './components/ExampleFor.vue';
 </script>
 
 <template>
   <h1>Hi there</h1>
-  <!-- <CounterOption />
-  <CounterComposition />
-  <PostContainer /> -->
-  <ExampleSelective />
+  <!-- <CounterOption /> -->
+  <!-- <CounterComposition /> -->
+  <!-- <PostContainer /> -->
+  <!-- <ExampleSelective /> -->
+  <ExampleFor />
 </template>
 
 <style scoped>
---
+++ src/components/ExampleFor.vue
@@ -0,0 +1,32 @@
+<script setup>
+    import { ref } from 'vue';
+    let countries = ref([
+        { 
+            code: 'es',
+            name: 'Spain'
+        },
+        { 
+            code: 'uk',
+            name: 'United Kingdom'
+        },
+        { 
+            code: 'it',
+            name: 'Italy'
+        },
+        { 
+            code: 'pt',
+            name: 'Portugal'
+        }
+    ])
+</script>
+
+<template>
+    <ul>
+        <!-- Ponemos el index si no hay clave única para cada elemento, en este caso sí son únicos -->
+        <!-- <li v-for="(country, index) in countries" v-bind:key="index">{{ country }}</li> -->
+        <li v-for="country in countries" v-bind:key="country.code">{{ country.name }}</li>
+    </ul>
+</template>
+
+<style  scoped>
+</style>
