0802eea2891db88501c2513237eee2a2a5affb79
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2023-07-14 13:41:24 +0200

    custom directives

--- src/components/ComputedExercise.vue
+++ src/components/ComputedExercise.vue
@@ -8,9 +8,11 @@
 
 <template>
     <div>
-        <label  v-font-size>Subtotal</label>
+        <label v-font-size>Subtotal</label>
         <input type="number" v-model="subTotal">
-        <h3>Total impuesto a pagar: {{ totalVat }}({{ vat }}%)</h3>
+        <h3 v-custom-size="40">Total impuesto a pagar: {{ totalVat }}({{ vat }}%)</h3>
+        <p v-argument-size:xxl>Probando directiva custom con argumentos</p>
+        <p v-custom-font.xxl.red>Directiva custom con modificadores</p>
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -11,4 +11,57 @@ app.directive('font-size', {
     }
 });
 
+app.directive('custom-size', {
+    beforeMount: (el, binding) =>{
+        el.style.fontSize = binding.value + 'px';
+    }
+});
+
+app.directive('argument-size', {
+    beforeMount: (el, binding) =>{
+        let size = 18;
+        switch(binding.arg){
+            case 'sm': 
+                size = 10
+                break
+            case 'md':
+                size = 18
+                break
+            case 'lg':
+                size = 25
+                break
+            case 'xl':
+                size = 40
+                break
+            case 'xxl':
+                size = 72
+        }
+        el.style.fontSize = size + 'px';
+    }
+});
+
+app.directive('custom-font', {
+    beforeMount: (el, binding) =>{
+        let size = 18;
+        
+        if(binding.modifiers.sm){
+            size = 10;
+        } else if(binding.modifiers.lg){
+            size = 25;
+        } else if(binding.modifiers.xxl){
+            size = 72;
+        };
+
+        el.style.fontSize = size + 'px';
+
+        if(binding.modifiers.red){
+            el.style.color = 'red';
+        } else if(binding.modifiers.green){
+            el.style.color = 'green';
+        } else if(binding.modifiers.blue){
+            el.style.color = 'blue';
+        };
+    }
+});
+
 app.mount('#app');
